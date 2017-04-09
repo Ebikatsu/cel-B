04627a2ea05b2abadac56a55ce9df02838dd8a6b
Naoki Watanabe <Ebikatsu@users.noreply.github.com>
2017-04-10 07:53:33 +0900

    2017/4/10 USBメモリへのコピー機能追加

--- pdf.sh
+++ pdf.sh
@@ -4,37 +4,52 @@
     filename=${basename%.*} # 拡張子を取った名前
     pdfflag=1
     dviflag=1
+		path1=/media/s153109/A464-BC88/cel-B/my-reports # 大学のPCのUSBメモリへのパス
+		path2=/media/watanabe/A464-BC88/cel-B/my-reports # 個人のPCのUSBメモリへのパス
+
     
 if [ -a ${filename}.tex ]; then
     if [ -a ${filename}.tex ]; then
 	# tex -> dviファイル
 	platex ${filename}.tex
     else
-	echo "ファイル：${filename}.tex が存在しません"
-	dviflag=0
-	pdfflag=0
+			echo "ファイル：${filename}.tex が存在しません"
+			dviflag=0
+			pdfflag=0
     fi
     
     if [ -a ${filename}.dvi ]; then
-	# dvi -> pdfファイル
-	dvipdfmx ${filename}.dvi
+			# dvi -> pdfファイル
+			dvipdfmx ${filename}.dvi
     elif [ $dviflag != 0 ]; then
-	echo "${filename}.dvi が作成できていないようです．"
-	pdfflag=0
+			echo "${filename}.dvi が作成できていないようです．"
+			pdfflag=0
     fi
 
     if [ -a ${filename}.pdf ]; then
-	# pdfファイルを開く
-	evince ${filename}.pdf 1>/dev/null 2>&1 &
-	if [ -a $/media/s153109/A464-BC88 ]; then
-	    # USBメモリにコピー
-	    cp ./${filename}.pdf /media/s153109/A464-BC88/cel-B/my-reports/${filename}.pdf
-	    echo "USBメモリにコピーしました."
-	elif
-	    echo "USBメモリが挿入されていません.";
-	    echo "コピーされませんでした."
-	fi    
+			# pdfファイルを開く
+			evince ${filename}.pdf 1>/dev/null 2>&1 &
+	
+			# USBメモリにコピー
+			if [ -d $path1 ]; then
+	    	cp ./${filename}.tex $path1/${filename}.tex
+	    	cp ./${filename}.pdf $path1/${filename}.pdf
+	    	echo "USBメモリにコピーしました."
+			elif [ -d $path2 ]; then
+	    	ls $path2
+				cp ./${filename}.tex $path2/${filename}.tex
+	    	cp ./${filename}.pdf $path2/${filename}.pdf
+	    	echo "USBメモリにコピーしました."
+			else
+	    	echo "USBメモリが挿入されていません.";
+	    	echo "コピーされませんでした."
+			fi    
     elif [ $pdfflag != 0 ]; then
-	echo "${filename}.pdf が開けません．"
+			echo "${filename}.pdf が開けません．"
     fi
+
+    # 不要なファイルを削除
+    rm ${filename}.aux
+    rm ${filename}.dvi
+    rm ${filename}.log    
 fi
